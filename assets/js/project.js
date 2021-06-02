1a00fd19739d5e79dd9150190969e02f2e0a0e47
JamesMcQueen2020 <60991590+JamesMcQueen2020@users.noreply.github.com>
2021-06-02 23:01:07 +0100

    Description Display for Projects

--- assets/js/project.js
+++ assets/js/project.js
@@ -108,7 +108,7 @@ const projects = [
 const showCards = () => {
   let output = "";
   projects.forEach(
-    ({ title, cardImage, tags, Previewlink, Githublink }) => {
+    ({ title, cardImage, description, tags, Previewlink, Githublink }) => {
       (output += `       
     <div class="column skill-card card">
       <div class="wrapper" style="background: url(${cardImage}) center / cover no-repeat;">
@@ -118,6 +118,7 @@ const showCards = () => {
           <div class="content">
           <div class="title-div">
             <h1 class="title"><a href="#">${title}</a></h1>
+            <h2 ><a>${description}</a></h2>
             </div>
         <ul class="menu-content"><br>
               <li><a href="${Previewlink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-monitor"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg></a></li>
